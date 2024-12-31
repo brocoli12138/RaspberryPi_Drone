eede33b9e4def2477cd29a0d717fb33aa053fc0d
brocoli12138 <3114563956@qq.com>
2025-01-01 01:32:38 +0800

    reference manual refresh

---
+++ system/mqtt.h
@@ -0,0 +1,22 @@
+#ifndef __MQTT_H__
+#define __MQTT_H__
+
+class Mqtt
+{
+private:
+    /* data */
+public:
+    Mqtt(/* args */);
+    ~Mqtt();
+};
+
+Mqtt::Mqtt(/* args */)
+{
+}
+
+Mqtt::~Mqtt()
+{
+}
+
+
+#endif
