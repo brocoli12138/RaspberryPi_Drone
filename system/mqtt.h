980b013417f1d0218f7eaf2f9f4beb13becf3fae
brocoli12138 <3114563956@qq.com>
2025-01-01 09:46:53 +0800

    mqtt class frame renew

--- system/mqtt.h
+++ system/mqtt.h
@@ -1,6 +1,21 @@
 #ifndef __MQTT_H__
 #define __MQTT_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "MQTTAsync.h"
+
+#if !defined(_WIN32)
+#include <unistd.h>
+#else
+#include <windows.h>
+#endif
+
+#if defined(_WRS_KERNEL)
+#include <OsWrapper.h>
+#endif
+
 class Mqtt
 {
 private:
@@ -17,6 +32,5 @@ Mqtt::Mqtt(/* args */)
 Mqtt::~Mqtt()
 {
 }
-
 
 #endif
