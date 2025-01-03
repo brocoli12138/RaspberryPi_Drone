176252996196b690464decda9f892ae9973642e8
brocoli12138 <3114563956@qq.com>
2025-01-04 01:09:44 +0800

    The first self repackaged MQTTClient submission

--- system/mqtt.h
+++ system/mqtt.h
@@ -1,68 +1,118 @@
 #ifndef __MQTT_H__
 #define __MQTT_H__
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "MQTTAsync.h"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include "mqtt/async_client.h"
 
-#if !defined(_WIN32)
-#include <unistd.h>
-#else
-#include <windows.h>
-#endif
 
-#if defined(_WRS_KERNEL)
-#include <OsWrapper.h>
-#endif
+// The will message when connection is interrupted
+const char *LWT_PAYLOAD = "Last will and testament.";
 
-#define TIMEOUT 10000L
-class Mqtt
+
+// Changes may take place anytime when new mqtt messages arrive.
+mqtt::const_message_ptr NEWEST_MESSAGE;
+
+/////////////////////////////////////////////////////////////////////////////
+
+/**
+ * A Callback class for use with the main MQTT client.
+ */
+class Callback : public virtual mqtt::callback
 {
 private:
-    MQTTAsync _client;
-    MQTTAsync_connectOptions _conn_opts;
-    MQTTAsync_disconnectOptions _disc_opts;
-    MQTTAsync_message _pub_msg;
-    MQTTAsync_responseOptions _resp_opts;
-
+    /* data */
 public:
-    Mqtt(const char *,const char *);
-    ~Mqtt();
+    Callback(/* args */){};
+    /**
+     * This method is called when the client is connected.
+     * Note that, in response to an initial connect(), the token from the
+     * connect call is also signaled with an on_success(). That occurs just
+     * before this is called.
+     */
+    void connected(const std::string & /*cause*/) override
+    {
+        std::cout << "\nConnection success!" << std::endl;
+    };
+    /**
+     * This method is called when the connection to the server is lost.
+     */
+    void connection_lost(const std::string &cause) override
+    {
+        std::cout << "\nConnection lost" << std::endl;
+        if (!cause.empty())
+            std::cout << "\tcause: " << cause << std::endl;
+    };
+    /**
+     * This method is called when a message arrives from the server.
+     */
+    void message_arrived(mqtt::const_message_ptr msg) override
+    {
+        NEWEST_MESSAGE = msg;
+    };
+    /**
+     * Called when delivery for a message has been completed, and all
+     * acknowledgments have been received.
+     */
+    void delivery_complete(mqtt::delivery_token_ptr tok) override
+    {
+        std::cout << "\tDelivery complete for token: "
+                  << (tok ? tok->get_message_id() : -1) << std::endl;
+    };
+
+    ~Callback(){};
 };
 
-Mqtt::Mqtt(const char * brokerAddress,const char *clientID) : _client(), _conn_opts(MQTTAsync_connectOptions_initializer),
-               _disc_opts(MQTTAsync_disconnectOptions_initializer),
-               _pub_msg(MQTTAsync_message_initializer),
-               _resp_opts(MQTTAsync_responseOptions_initializer)
+class Mqttclient : public mqtt::async_client
 {
-    int rc;
-    if ((rc = MQTTAsync_create(&_client, brokerAddress, clientID, MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTASYNC_SUCCESS)
-    {
-        printf("Failed to create client, return code %d\n", rc);
-        rc = EXIT_FAILURE;
-    }
+private:
+    Callback _callback;
+    mqtt::connect_options _connectOption;
+    mqtt::token_ptr _connectToken;
+    mqtt::topic _publishTopic;
+    std::string _serverURI;
 
-    if ((rc = MQTTAsync_setCallbacks(_client, _client, connlost, msgarrvd, NULL)) != MQTTASYNC_SUCCESS)
+public:
+    Mqttclient(const std::string &serverURI, const std::string &clientId, const std::string &publishTopic, int qos);
+    std::string get_newest_message()
     {
-        printf("Failed to set callbacks, return code %d\n", rc);
-        rc = EXIT_FAILURE;
-    }
+        return NEWEST_MESSAGE->to_string();
+    };
+    // using default subscribe
+    // token_ptr subscribe(const string& topicFilter, int qos) override;
+    mqtt::delivery_token_ptr publish(mqtt::binary_ref payload, int qos, bool retained = false)
+    {
+        return _publishTopic.publish(payload, qos, retained);
+    };
+    ~Mqttclient();
+};
 
-    _conn_opts.keepAliveInterval = 20;
-    _conn_opts.cleansession = 1;
-    _conn_opts.onSuccess = onConnect;
-    _conn_opts.onFailure = onConnectFailure;
-    _conn_opts.context = _client;
-    if ((rc = MQTTAsync_connect(client, &_conn_opts)) != MQTTASYNC_SUCCESS)
+Mqttclient::Mqttclient(const std::string &serverURI, const std::string &clientId, const std::string &publishTopic, int qos)
+    : async_client(serverURI, clientId, 128, nullptr), _callback(), _publishTopic(*this, publishTopic, qos), _serverURI(serverURI),
+      _connectOption(mqtt::connect_options_builder().clean_session().v5().will(mqtt::message("wills", LWT_PAYLOAD, strlen(LWT_PAYLOAD), 1, false)).automatic_reconnect(std::chrono::seconds(1), std::chrono::seconds(10)).finalize()),
+      _connectToken()
+{
+    set_callback(_callback);
+    _connectToken = connect(_connectOption);
+    _connectToken->wait();
+    auto rsp = _connectToken->get_connect_response();
+    // Make sure we were granted a v5 connection.
+    if (rsp.get_mqtt_version() < MQTTVERSION_5)
     {
-        printf("Failed to start connect, return code %d\n", rc);
-        rc = EXIT_FAILURE;
+        throw mqtt::security_exception{1, std::string{"Did not get an MQTT v5 connection."}};
     }
 }
 
-Mqtt::~Mqtt()
+Mqttclient::~Mqttclient()
 {
 }
+
+/////////////////////////////////////////////////////////////////////////////
 
 #endif
---
+++ system/mqtttest.cpp
@@ -0,0 +1,17 @@
+#include "mqtt.h"
+
+// The QoS for sending data
+const int QOS = 1;
+const std::string DFLT_SERVER_ADDRESS{"mqtt://localhost:1883"};
+const std::string CLIENT_ID{"paho_cpp_async_publish"};
+const std::string PUBTOPIC{"data/video"};
+const std::string SUBTOPIC{"data/control"};
+int main()
+{
+    char msg[]{0x01, 0x02, 0x03};
+    Mqttclient client{DFLT_SERVER_ADDRESS, CLIENT_ID, PUBTOPIC, QOS};
+    client.subscribe(SUBTOPIC, QOS);
+    client.publish(msg, QOS);
+    std::cout << client.get_newest_message();
+    return 0;
+}
